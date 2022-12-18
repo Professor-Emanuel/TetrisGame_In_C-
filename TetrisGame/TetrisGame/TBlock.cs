092bbec42c25a29333aab2d1968a3d7bb11baffe
Professor-Emanuel <87651738+Professor-Emanuel@users.noreply.github.com>
2022-12-18 17:40:25 +0200

    Add files via upload

---
+++ TetrisGame/TetrisGame/TBlock.cs
@@ -0,0 +1,17 @@
+﻿namespace TetrisGame
+{
+    public class TBlock : Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[]{new(0,1), new(1,0), new(1,1), new(1,2)},
+            new Position[]{new(0,1), new(1,1), new(1,2), new(2,1)},
+            new Position[]{new(1,0), new(1,1), new(1,2), new(2,1)},
+            new Position[]{new(0,1), new(1,0), new(1,1), new(2,1)}
+        };
+
+        public override int Id => 6; //blocks to spown in the middle of the top row
+        protected override Position StartOffset => new Position(0, 3); //tiles array above
+        protected override Position[][] Tiles => tiles;
+    }
+}
